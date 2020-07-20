d5f44c713b3b00195c59505418b21f465346c4e3
Tawanda Kanyangarara <kliation@gmail.com>
2020-07-20 06:00:22 +0100

    initial

---
+++ src/App.tsx
@@ -0,0 +1,129 @@
+import React from 'react';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div id="wrapper" className="App">
+      
+			
+
+				
+					<section className="intro">
+						<header>
+							<h1>WK Designs</h1>
+							<p>bty Wilona Wilson</p>
+							<ul className="actions">
+								<li><a href="#first" className="arrow scrolly"><span className="label">Next</span></a></li>
+							</ul>
+						</header>
+						<div className="content">
+							<span className="image fill" data-position="center"><img src="images/pic01.jpg" alt="" /></span>
+						</div>
+					</section>
+{/* 
+				
+					<section id="first">
+						<header>
+							<h2>Magna sed nullam nisl adipiscing</h2>
+						</header>
+						<div className="content">
+							<p><strong>Lorem ipsum dolor</strong> sit amet consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor amet fermentum. Nullam venenatis erat id vehicula ultrices sed ultricies condimentum. Magna sed etiam consequat, et lorem adipiscing sed nulla. Volutpat nisl et tempus et dolor libero, feugiat magna tempus, sed et lorem adipiscing.</p>
+							<span className="image main"><img src="./images/pic02.jpg" alt="" /></span>
+						</div>
+					</section>
+
+				
+					<section>
+						<header>
+							<h2>Feugiat consequat tempus ultrices</h2>
+						</header>
+						<div className="content">
+							<p><strong>Etiam tristique libero</strong> eu nibh porttitor amet fermentum. Nullam venenatis erat id vehicula ultrices sed ultricies condimentum.</p>
+							<ul className="feature-icons">
+								<li className="icon solid fa-laptop">Consequat tempus</li>
+								<li className="icon solid fa-bolt">Etiam adipiscing</li>
+								<li className="icon solid fa-signal">Libero nullam</li>
+								<li className="icon solid fa-cog">Blandit condimentum</li>
+								<li className="icon solid fa-map-marker-alt">Lorem ipsum dolor</li>
+								<li className="icon solid fa-code">Nibh amet venenatis</li>
+							</ul>
+							<p>Vehicula ultrices sed ultricies condimentum. Magna sed etiam consequat, et lorem adipiscing sed nulla. Volutpat nisl et tempus et dolor libero, feugiat magna tempus, sed et lorem adipiscing.</p>
+						</div>
+					</section>
+
+				
+					<section>
+						<header>
+							<h2>Ultrices erat magna sed condimentum</h2>
+						</header>
+						<div className="content">
+							<p><strong>Integer mollis egestas</strong> nam maximus erat id euismod egestas. Pellentesque sapien ac quam. Lorem ipsum dolor sit nullam.</p>
+
+							
+								<section>
+									<header>
+										<h3>Erat aliquam</h3>
+										<p>Vehicula ultrices dolor amet ultricies et condimentum. Magna sed etiam consequat, et lorem adipiscing sed dolor sit amet, consectetur amet do eiusmod tempor incididunt  ipsum suspendisse ultrices gravida.</p>
+									</header>
+									<div className="content">
+										<div className="gallery">
+											<a href="images/gallery/fulls/01.jpg" className="landscape"><img src="images/gallery/thumbs/01.jpg" alt="" /></a>
+											<a href="images/gallery/fulls/02.jpg"><img src="images/gallery/thumbs/02.jpg" alt="" /></a>
+											<a href="images/gallery/fulls/03.jpg"><img src="images/gallery/thumbs/03.jpg" alt="" /></a>
+											<a href="images/gallery/fulls/04.jpg" className="landscape"><img src="images/gallery/thumbs/04.jpg" alt="" /></a>
+										</div>
+									</div>
+								</section>
+
+							
+								<section>
+									<header>
+										<h3>Nisl consequat</h3>
+										<p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam sed facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet mauris. Ut magna finibus nisi nec lacinia ipsum maximus.</p>
+									</header>
+									<div className="content">
+										<div className="gallery">
+											<a href="images/gallery/fulls/05.jpg" className="landscape"><img src="images/gallery/thumbs/05.jpg" alt="" /></a>
+											<a href="images/gallery/fulls/06.jpg"><img src="images/gallery/thumbs/06.jpg" alt="" /></a>
+											<a href="images/gallery/fulls/07.jpg"><img src="images/gallery/thumbs/07.jpg" alt="" /></a>
+										</div>
+									</div>
+								</section>
+
+							
+								<section>
+									<header>
+										<h3>Lorem gravida</h3>
+										<p>Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aenean ornare velit lacus, ac varius sed enim lorem ullamcorper dolore.  ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis.</p>
+									</header>
+									<div className="content">
+										<div className="gallery">
+											<a href="images/gallery/fulls/08.jpg" className="portrait"><img src="images/gallery/thumbs/08.jpg" alt="" /></a>
+											<a href="images/gallery/fulls/09.jpg" className="portrait"><img src="images/gallery/thumbs/09.jpg" alt="" /></a>
+											<a href="images/gallery/fulls/10.jpg" className="landscape"><img src="images/gallery/thumbs/10.jpg" alt="" /></a>
+										</div>
+									</div>
+								</section>
+
+						</div>
+					</section>
+
+				
+					<section>
+						<header>
+							<h2>Duis sed adpiscing veroeros amet</h2>
+						</header>
+						<div className="content">
+							<p><strong>Proin tempus feugiat</strong> sed varius enim lorem ullamcorper dolore aliquam aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore.</p>
+							<ul className="actions">
+								<li><a href="www" className="button primary large">Get Started</a></li>
+								<li><a href="www" className="button large">Learn More</a></li>
+							</ul>
+						</div>
+					</section> */}
+    </div> 
+  );
+}
+
+export default App;
